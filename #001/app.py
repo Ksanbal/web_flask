df972213dfebeb092bac8e2eb0f9a38c1ebe4c57
Ksanbal <hyungyun96@gmail.com>
2019-05-20 19:03:46 +0900

    Add first_page()

--- #001/app.py
+++ #001/app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/') # 아무것도 입력하지 않았을 때 상태.
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/<a>') # /뒤로 입력받는 문자를 표시하는 페이지
+def first_page(a):
+    print(dir(request.user_agent))
+    return render_template('Index.html', a=a)
 
 
 if __name__ == '__main__':
