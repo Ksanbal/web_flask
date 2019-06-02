77d5c43b01ae5994ee4a4ecece0c87446c8548a5
김산발 <hyungyun96@gmail.com>
2019-06-02 16:11:58 +0900

    Correct named with PEP8

--- Flask_example/app.py
+++ Flask_example/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_admin import AdminIndexView #Admin 처음 페이지에서 보여주는
+from flask_admin import AdminIndexView #Admin 페이지 Home
 from flask_admin.contrib.sqlamodel import ModelView
 from extesnsions import admin, db
 from models import User
@@ -11,14 +11,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_FILE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '123456790'
-app.config['FLASK_ADMIN_SWATCH'] = 'cerulean' #부트스트립에서 지원하는 기
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'  # 부트스트립에서 지원하는 테마
 
 
 db.init_app(app)
-admin.init_app(app) # init를 통해 Flask와 연동하겠다
+admin.init_app(app)  # init를 통해 Flask와 연동하겠다
 
 
-class UserModelview(ModelView):  #Admin 페이지에서 보이는 기능 수정
+class UserModelView(ModelView):  # Admin 페이지에서 보이는 기능 수정
     can_create = True
     can_edit = True
     can_delete = True
@@ -27,7 +27,7 @@ class UserModelview(ModelView):  #Admin 페이지에서 보이는 기능 수정
     create_modal = True
 
 
-admin.add_view(UserModelview(User, db.session))
+admin.add_view(UserModelView(User, db.session))
 
 
 # adminIndexview = AdminIndexView(name='admin2') #Admin의 Home 페이지 수정
@@ -35,11 +35,11 @@ admin.add_view(UserModelview(User, db.session))
 
 
 @app.before_first_request
-def dbCreat():
+def db_creat():
     db.create_all()
 
 
-@app.route('/')
+@app.route('/')  # 첫화면
 def hello_world():
     return 'Hello World!'
 
