ceaffe0a85d27a22fda6ac2db196216ada75d750
김산발 <hyungyun96@gmail.com>
2019-06-14 21:02:31 +0900

    Delete files and add #004

--- Flask_example/config.py
+++
@@ -1,1 +0,0 @@
-DATABASE_FILE = 'sqlite:///test.db'
--- Flask_example/extesnsions.py
+++
@@ -1,8 +0,0 @@
-#설치전용 파일
-
-from flask_admin import Admin
-from flask_sqlalchemy import SQLAlchemy
-
-
-admin = Admin(name="Admin : DB Views", template_mode='bootstrap3')
-db = SQLAlchemy()
